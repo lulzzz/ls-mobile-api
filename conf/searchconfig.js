349ab639605ed3b58d2c9c67d216c2e2c25f2eab
yuvaraj <yuvaraj@logistimo.com>
2017-05-11 14:45:10 +0530

    LSMOB-17 Material Search API code review changes

---
+++ conf/searchconfig.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yuvaraj on 11/05/17.
+ */
+module.exports = {
+    material_search:{
+        url: '/materials/',
+        method:'GET',
+        timeout:1000,
+        req_pld: 'query'
+    }
+}
