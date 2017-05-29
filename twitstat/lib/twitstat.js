1f26d6e4beac8c7b605c10f9330d96dc35f11954
yuvaraj <yuvaraj@logistimo.com>
2017-05-29 11:07:24 +0530

    LSMOB-28 Sample test case created , not integrated with API

---
+++ twitstat/lib/twitstat.js
@@ -0,0 +1,29 @@
+/**
+ * Created by yuvaraj on 29/05/17.
+ */
+var request = require('request');
+
+module.exports = {
+    getPopularity: function (url, callback) {
+        var endpoint = "http://urls.api.twitter.com/1/urls/count.json?url=" + url;
+        request(endpoint, function (err, response, body) {
+            if (err) {
+                return callback(err, null);
+            }
+            var twitterResponse = JSON.parse(body);
+            var popularity = "MEDIUM";
+            if (twitterResponse.count < 10) {
+                popularity = "LOW";
+            } else if (twitterResponse.count > 50) {
+                popularity = "HIGH";
+            }
+
+            var stat = JSON.stringify({
+                "url": twitterResponse.url,
+                "popularity": popularity
+            });
+
+            return callback(null, stat);
+        });
+    }
+};
