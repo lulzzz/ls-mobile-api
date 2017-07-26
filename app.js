bedae87192e3d1af5b914a69c35af6065aa611c1
Charan Malemarpuram <charan@logistimo.com>
2017-07-26 11:20:42 +0530

    metrics correction (#39)

--- app.js
+++ app.js
@@ -19,17 +19,12 @@ collectDefaultMetrics({timeout: 5000});
 
 var app = express();
 //logging
-app.use(require('morgan')({ "stream": logger.stream }));
+app.use(require('morgan')(':remote-addr :remote-user :method :url HTTP/:http-version :status :res[content-length] - :response-time ms ":user-agent"', {"stream": logger.stream}));
 //to parse request body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set('x-powered-by', false);
-
-app.use(function(req,res,next) {
-    logger.info("Received request for resource :" + req.url);
-  next();
-});
 
 app.use('/', interceptor);
 
@@ -39,6 +34,7 @@ app.use(route);
 // if control reaches here means no
 // appropriate req handler not found
 app.use(function(req,res) {
+    metrics.incrementCounter(req.method, 404, req.path);
   res.status(404).send('Resource Not Found');
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,6 @@
 var router = require('express').Router();
 var logger = require('../lib/utils/log');
 
-router.use(function (req, res, next) {
-    logger.info("Main route executing for req url:" + req.url);
-    //calling middleware
-    return next();
-});
-
 router.use('/auth/*', require('./auth'));
 router.use('/dashboards/*', require('./dashboard'));
 router.use('/inventory*', require('./inventory'));
