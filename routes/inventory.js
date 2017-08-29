1922ea822cc3fb28122d6e8a45100292520be9ae
kgaurav1 <kumar@logistimo.com>
2017-08-29 14:21:45 +0530

    Emptyarray fix (#57)
    * fixed
    
    * fixed
    
    * fixed
    
    * fixed
    
    * fixed

--- routes/inventory.js
+++ routes/inventory.js
@@ -19,7 +19,7 @@ router.get('/inventory', function (req, res) {
     return new Promise(function (resolve, reject) {
         try {
             validate(req);
-        } catch(exception){
+        } catch (exception) {
             logger.error(exception);
             reject(exception);
             return;
@@ -36,33 +36,35 @@ router.get('/inventory', function (req, res) {
                 invData.total = data.numFound;
                 invData.size = data.size;
                 invData.offset = data.offset;
-                data.results.forEach(function(data){
-                    var dt ={};
-                    dt.mId = data.mId;
-                    dt.mnm = data.mnm;
-                    dt.eId = data.kId;
-                    dt.tc = data.stk;
-                    dt.a = data.astk;
-                    dt.av = data.atpsk;
-                    dt.it = data.tstk;
-                    dt.min = data.reord;
-                    dt.max= data.max;
-                    dt.lu= data.t;
-                    dt.se = data.event;
-                    dt.enm = data.enm;
-                    dt.ed=data.period;
-                    dt.loc = {};
-                    dt.loc.st = data.em.st;
-                    dt.loc.stid = data.em.stid;
-                    dt.loc.tlk = data.em.tlk;
-                    dt.loc.ctr = data.em.ctr;
-                    dt.loc.ctrid = data.em.ctrid;
-                    dt.loc.ct = data.em.ct;
-                    dt.loc.ctid = data.em.ctid;
-                    dt.loc.ds = data.em.ds;
-                    dt.loc.dsid = data.em.dsid;
-                    invData.data.push(dt);
-                });
+                if (commonUtils.checkNotNullEmptyArray(data.results)) {
+                    data.results.forEach(function (data) {
+                        var dt = {};
+                        dt.mId = data.mId;
+                        dt.mnm = data.mnm;
+                        dt.eId = data.kId;
+                        dt.tc = data.stk;
+                        dt.a = data.astk;
+                        dt.av = data.atpsk;
+                        dt.it = data.tstk;
+                        dt.min = data.reord;
+                        dt.max = data.max;
+                        dt.lu = data.t;
+                        dt.se = data.event;
+                        dt.enm = data.enm;
+                        dt.ed = data.period;
+                        dt.loc = {};
+                        dt.loc.st = data.em.st;
+                        dt.loc.stid = data.em.stid;
+                        dt.loc.tlk = data.em.tlk;
+                        dt.loc.ctr = data.em.ctr;
+                        dt.loc.ctrid = data.em.ctrid;
+                        dt.loc.ct = data.em.ct;
+                        dt.loc.ctid = data.em.ctid;
+                        dt.loc.ds = data.em.ds;
+                        dt.loc.dsid = data.em.dsid;
+                        invData.data.push(dt);
+                    });
+                }
                 resolve(invData);
             }
         });
@@ -70,7 +72,7 @@ router.get('/inventory', function (req, res) {
 });
 
 router.get('/inventory/entity/:entity_id/:material_id', function (req, res) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         var queryModel = queryBuilder.buildInvDetailParams(req);
         invDetail.getSingleInvDetail(queryModel, req, res, function (err, data) {
             if (err) {
@@ -83,17 +85,17 @@ router.get('/inventory/entity/:entity_id/:material_id', function (req, res) {
     });
 });
 
-function validate(req){
-    if(commonUtils.checkNull(req.query.entity_id) && commonUtils.checkNull(req.query.material_id) ){
+function validate(req) {
+    if (commonUtils.checkNull(req.query.entity_id) && commonUtils.checkNull(req.query.material_id)) {
         commonUtils.generateValidationError("One of entity id or material id is required.");
     }
-    if(commonUtils.checkNotNullEmpty(req.query.entity_id) && commonUtils.checkNotNullEmpty(req.query.material_id) ){
+    if (commonUtils.checkNotNullEmpty(req.query.entity_id) && commonUtils.checkNotNullEmpty(req.query.material_id)) {
         commonUtils.generateValidationError("Any one of entity id or material id is required.");
     }
-    if(commonUtils.checkNotNullEmpty(req.query.entity_id) && commonUtils.checkNotNullEmpty(req.query.etags)){
+    if (commonUtils.checkNotNullEmpty(req.query.entity_id) && commonUtils.checkNotNullEmpty(req.query.etags)) {
         commonUtils.generateValidationError("Any one of entity id or entity tag is required.");
     }
-    if(commonUtils.checkNotNullEmpty(req.query.material_id) && commonUtils.checkNotNullEmpty(req.query.mtags)){
+    if (commonUtils.checkNotNullEmpty(req.query.material_id) && commonUtils.checkNotNullEmpty(req.query.mtags)) {
         commonUtils.generateValidationError("Any one of material id or material tag is required.");
     }
     if (commonUtils.checkNotNullEmpty(req.query.entity_id) && (commonUtils.checkNotNullEmpty(req.query.state) ||
