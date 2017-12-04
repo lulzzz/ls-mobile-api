7097ed948a75b0bcdadc7397ffece91a154677c4
SmritiLogistimo <smriti@logistimo.com>
2017-12-04 17:15:49 +0530

    Caching event summary data to fetch event summaries faster

--- routes/events.js
+++ routes/events.js
@@ -5,9 +5,10 @@ var path = require('path'),
     logger = require(path.resolve('./lib/utils/logger', '')),
     decoder = require(path.resolve('./lib/utils/urldecoder', '')),
     service = require(path.resolve('./lib/restclient/events/eventService', '')),
-    utils = require(path.resolve('./lib/utils/common/common-utils','')),
-    domainCfgService = require(path.resolve('./lib/restclient/domain/domainCfgService','')),
-    eventResBuilder = require(path.resolve('./lib/builder/eventResBuilder',''));
+    utils = require(path.resolve('./lib/utils/common/common-utils', '')),
+    domainCfgService = require(path.resolve('./lib/restclient/domain/domainCfgService', '')),
+    eventResBuilder = require(path.resolve('./lib/builder/eventResBuilder', '')),
+    redisService = require(path.resolve('./lib/redis/redisService', ''));
 
 router.use(function (req, res, next) {
     req.url = decoder.decodeurl(req);
@@ -18,33 +19,50 @@ router.get('/event-summaries', function (req) {
     try {
         validateRequestParams(req);
     } catch (exception) {
-        logger.error(exception);
+        logger.warn(exception);
         reject(exception);
         return;
     }
     return new Promise(function (resolve, reject) {
-        service.getEventSummaries(req, function (err, data) {
+        var key = "event" + "_" + req.query.cn_domain_id;
+        if (utils.checkNotNullEmpty(req.query.event_id)) {
+            key = key + "_" + req.query.event_id;
+        }
+        redisService.getData(key, function (err, cacheData) {
             if (err) {
-                logger.error("Error while fetching event summaries for domain: " + req.query.cn_domain_id);
-                reject(err);
+                // do nothing
             } else {
-                if (req.query.include_distribution) {
-                    var eventData = JSON.parse(data);
-                    if(utils.checkNotNullEmptyArray(eventData.summaries)) {
-                        var objectIds = eventResBuilder.getDistributionObjectIds(eventData);
-                        domainCfgService.get(req, objectIds, function (err, domainMetaData) {
-                            if (err) {
-                                logger.warn("\n Error while fetching the domain general config for the object ids: \n" + exception);
-                                reject(exception);
-                            } else {
-                                resolve(eventResBuilder.buildEventSummary(JSON.parse(domainMetaData), eventData));
-                            }
-                        });
-                    } else {
-                        resolve(JSON.parse(eventData));
-                    }
+                if (utils.checkNotNullEmpty(cacheData)) {
+                    resolve(JSON.parse(cacheData));
                 } else {
-                    resolve(JSON.parse(data));
+                    service.getEventSummaries(req, function (err, data) {
+                        if (err) {
+                            logger.error("Error while fetching event summaries for domain: " + req.query.cn_domain_id);
+                            reject(err);
+                        } else {
+                            if (req.query.include_distribution) {
+                                var eventData = JSON.parse(data);
+                                if (utils.checkNotNullEmptyArray(eventData.summaries)) {
+                                    var objectIds = eventResBuilder.getDistributionObjectIds(eventData);
+                                    domainCfgService.get(req, objectIds, function (err, domainMetaData) {
+                                        if (err) {
+                                            logger.error("\n Error while fetching the domain general config for the object ids: \n" + exception);
+                                            reject(exception);
+                                        } else {
+                                            resolve(eventResBuilder.buildEventSummary(JSON.parse(domainMetaData), eventData));
+                                        }
+                                    });
+                                } else {
+                                    redisService.setData(key, data, parseInt((new Date().setHours(23, 59, 59, 999) - new Date()) / 1000));
+                                    resolve(JSON.parse(eventData));
+                                }
+                            } else {
+                                redisService.setData(key, data, parseInt((new Date().setHours(23, 59, 59, 999) - new Date()) / 1000));
+                                resolve(JSON.parse(data));
+                            }
+                        }
+                    });
+
                 }
             }
         });
@@ -54,12 +72,12 @@ router.get('/event-summaries', function (req) {
 router.get('/event-summaries/:event_id', function (req) {
     try {
         validateRequestParams(req);
-    } catch(exception) {
-        logger.error(exception);
+    } catch (exception) {
+        logger.warn(exception);
         reject(exception);
         return;
     }
-    return new Promise(function (resolve,reject) {
+    return new Promise(function (resolve, reject) {
         service.getEventsByType(req, function (err, data) {
             if (err) {
                 logger.error("Error while fetching event summaries for domain " + req.query.cn_domain_id + " and event" + req.params.event_id);
@@ -73,17 +91,17 @@ router.get('/event-summaries/:event_id', function (req) {
 
 function validateRequestParams(req) {
     if (req.baseUrl.endsWith('event-summaries')) {
-        if(utils.checkNullEmpty(req.query.cn_domain_id)){
+        if (utils.checkNullEmpty(req.query.cn_domain_id)) {
             utils.generateValidationError("Current domain id is required.");
         }
-        if(utils.checkNullEmpty(req.query.user_id)){
+        if (utils.checkNullEmpty(req.query.user_id)) {
             utils.generateValidationError("user id is required.");
         }
     } else {
-        if(utils.checkNullEmpty(req.query.cn_domain_id)){
+        if (utils.checkNullEmpty(req.query.cn_domain_id)) {
             utils.generateValidationError("Current domain id is required.");
         }
-        if(utils.checkNullEmpty(req.query.user_id)){
+        if (utils.checkNullEmpty(req.query.user_id)) {
             utils.generateValidationError("user id is required.");
         }
         if (utils.checkNullEmpty(req.params.event_id)) {
